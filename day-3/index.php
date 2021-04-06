1bfd0c52c5a8c8d1351916620278d3d75b415e92
tna007 <anh.to@edu.bc.fi>
2021-04-06 18:47:54 +0300

    Validate widthdrawn and balance
    Validate both as integer, positive, widthdrawn <= balance

--- day-3/index.php
+++ day-3/index.php
@@ -17,11 +17,11 @@
            };
         };
 
-        echo validate_username('tna_007') .'<br>';
-        echo validate_username('tna.007') . '<br>';
-        echo validate_username('tna@007') . '<br>';
-        echo validate_username('') . '<br>';
-        echo validate_username('tna00777777777777777777777') . '<br>';
+        echo validate_username('tna_007') .'<br>'
+        . validate_username('tna.007') . '<br>'
+        . validate_username('tna@007') . '<br>'
+        . validate_username('') . '<br>'
+        . validate_username('tna00777777777777777777777') . '<br>';
         echo '<hr>';
 
         function validate_weekday($day) {
@@ -31,12 +31,12 @@
                     'max_range' => 6,
                 )
             );
-            
+
             if (!is_string($day)) {
                 return filter_var($day, FILTER_VALIDATE_INT, $options);
             } else {
                 return false;
-            }
+            };
         };
 
         echo '0 is a valid weekday: ' . validate_weekday(0) . //true
@@ -54,6 +54,34 @@
         . 'An empty string is a valid weekday: ' . validate_weekday('') . //false 
         '<br>'
         . 'A string is a valid weekday: ' . validate_weekday('5') . //false
+        '<br>';
+        echo '<hr>';
+
+        function validate_widthdraw_amount($amount, $balance) {
+            if (is_int($amount)
+                && is_int($balance)
+                && $amount <= $balance
+                && $amount > 0
+            ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        echo 'Able to withdraw 100 from an account of 1000 balance: ' . validate_widthdraw_amount(100, 1000) . // true
+        '<br>' .
+        'Able to withdraw 1000 from an account of 1000 balance: ' . validate_widthdraw_amount(1000, 1000) . // true
+        '<br>' .
+        'Able to withdraw -50 from an account of 1000 balance: ' . validate_widthdraw_amount(-50, 1000) . // false
+        '<br>' .
+        'Able to withdraw 1500 from an account of 1000 balance: ' . validate_widthdraw_amount(1500, 1000) . // false
+        '<br>' .
+        'Able to withdraw true from an account of true balance: ' . validate_widthdraw_amount(true, true) . // false
+        '<br>' .
+        'Able to withdraw 0 from an account of negative 100 balance: ' . validate_widthdraw_amount(0, -100) . // false
+        '<br>' .
+        'Able to withdraw null from an account of 0 balance: ' . validate_widthdraw_amount(null, 0) . // false
         '<br>';
     ?>
 </body>
